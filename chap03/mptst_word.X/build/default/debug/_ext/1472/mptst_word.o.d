ef36a0a250ba240b4d5c775608c0fa100309c9e8
Gus <aengus.crowley@gmail.com>
2020-01-22 18:38:47 -0800

    initial commit

---
+++ chap03/mptst_word.X/build/default/debug/_ext/1472/mptst_word.o.d
@@ -0,0 +1,2 @@
+build/default/debug/_ext/1472/mptst_word.o:  \
+../mptst_word.s 
---
+++ chap03/mptst_word.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=mptst_word.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/mptst_word.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=mptstword.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/mptstword.x.tar

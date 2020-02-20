7e4aa3050316bed02f7f9f100d93dc77c46f8bf0
Gus <aengus.crowley@gmail.com>
2020-02-19 16:52:43 -0800

    srry

--- lab_5/strdcase.X/strdcase.s
+++ lab_5/strdcase.X/strdcase.s
@@ -16,8 +16,12 @@ sz_2:   .space 16
 mplab_x_bug: .space 2
 
 ; Initialized constant data section.
-; The statement below is taken from DS70613C, page 4-21.
-.section .const, psv
+; The statement below is taken from DS70613C, page 4-21..section .const, psv
 sz_1_const: .asciz "Upper/lower."
 sz_2_const: .asciz "mIXeD CaSe.."
+ 
+ dostr: 
+    
+ main: 
+    
 

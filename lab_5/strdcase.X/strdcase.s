a0d1d9bd6913af8c0e029560e0d00e012ecebb86
Gus <aengus.crowley@gmail.com>
2020-02-19 17:48:19 -0800

    readded template

--- lab_5/strdcase.X/strdcase.s
+++ lab_5/strdcase.X/strdcase.s
@@ -21,31 +21,62 @@ mplab_x_bug: .space 2
 sz_1_const: .asciz "Upper/lower."
 sz_2_const: .asciz "mIXeD CaSe.."
  
+; Code
+; ====
+.text
+.global __reset
+__reset:
+; Initialize the stack pointer and stack limit register.
+mov #__SP_init, W15
+; __SPLIM_init is set by linker to be after allocated data.
+mov #__SPLIM_init, W0
+mov W0, SPLIM
+
+; Copy constant data to RAM.
+;; char sz_1[] = "Hello";
+;; char sz_1[] = "UPPER/lower";
+call init_variables       ;initialize strings
+; Now that everything is initialized, call main().
+rcall main
+; If main() exits, restart the processor.
+reset
+ 
+main: 
+  mov.b #sz_1, W0
+  mov.b W0, W1
+  mov.b #sz_2, W0
+  rcall dostr
+  done: 
+  goto done
+   
+    
  dostr: 
     
     mov [W0], W3
-    cp.b W3
+    cp0.b W3
     ;While start
     bra Z, dostr_While_end
     ;store value incase need unchagned
-    mov.b [W1], u8_c
+    mov.b [WREG], u8_c
     ;if start
     mov [W0], W3
     mov #0x2F, W2
     cp.b W3, W2
     bra GTU, dostr_think_more
-    bra LEU, dostr_done
+    bra LEU, dostr_else_done
     
     ;if(u8_c > 0x2f) == true
     dostr_think_more:
     mov [W0], W3
     mov #0x20, W2
     ior W3, W2, [W1]
+    bra dostr_done
     
     ;else
-    dostr_done: 
+    dostr_else_done: 
     mov.b u8_c, [W1]
     ;incriment W0
+    dostr_done:
     inc W0, W0
     inc W1, W1
     bra dostr
@@ -53,9 +84,4 @@ sz_2_const: .asciz "mIXeD CaSe.."
     return
     
     
- main: 
-    mov.b sz_2, W0
-    ;TODo FIX W0 moves.b
-    mov.b sz_1, W1
-    rcall dostr
 
